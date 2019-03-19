4878928d3058d8a2cbc2f9d50e6fbe42fd547d4c
Hans <chiuhans111@gmail.com>
2019-03-19 15:38:34 +0800

    [closing roll] 排版美化與動畫效果

--- Closing/main.js
+++ Closing/main.js
@@ -26,7 +26,10 @@ var app = new Vue({
                 'co-holder': "共同主辦",
                 'co-organizer': '協辦單位',
                 'level-1': '深耕級合作夥伴',
-                'level-2': '前瞻級合作夥伴'
+                'level-2': '前瞻級合作夥伴',
+                'level-3': '新芽級合作夥伴',
+                'thank': '特別感謝',
+
             }[level]
         }
     }
@@ -38,7 +41,7 @@ Promise.all([
     get('https://raw.githubusercontent.com/sitcon-tw/2019/master/static/json/individual-sponsor.json')
 ]).then(function (data) {
     var [sponsors, staff, individual] = data.map(x => JSON.parse(x))
-    
+
     console.log(sponsors)
 
     //
@@ -73,7 +76,7 @@ Promise.all([
     }, 0)
 })
 
-var max_pixel_per_second = 150;
+var max_pixel_per_second = 0;
 var pixel_per_second = 0;
 var container = document.querySelector('.container')
 var past = null
@@ -81,70 +84,103 @@ var scrollTop = 0
 
 
 var i, j,
-    tags, lastOutTag, nextOutTag
+    tags, lastOutTag, nextOutTag,
+    images, delayshows
 
 function setup() {
-    tags = Array.from(document.querySelectorAll('.tag')).map(tag => {
-        tag.anchor = tag.querySelector('.anchor')
-        tag.text = tag.querySelector('.text')
-        tag.rect = tag.anchor.getBoundingClientRect()
-        tag.text.style.top = tag.rect.y + 'px'
-        return tag
-    })
+    appelement = document.querySelector('#app')
+
+
+    // tags = Array.from(document.querySelectorAll('.tag')).map(tag => {
+    //     tag.anchor = tag.querySelector('.anchor')
+    //     tag.text = tag.querySelector('.text')
+    //     tag.rect = tag.anchor.getBoundingClientRect()
+    //     tag.text.style.top = tag.rect.y + 'px'
+    //     return tag
+    // })
+    //
+    // register delay show
+    //
+    delayshows = Array.from(document.querySelectorAll('.delay-show'))
 
     //
     // Wait for images to load
     //
     images = Array.from(document.querySelectorAll('img'))
-    images.map(img => {
+    images = images.map(target => {
+        let obj = { src: target.src }
+        let img = document.createElement('img')
+        img.src = target.src
         img.addEventListener('load', () => {
-            img.done = true
-
+            obj.done = true
+            // console.log('loaded', img.src, images.filter(x => !x.done))
             if (images.every(x => x.done)) {
+                appelement.scrollTop = 0
                 update()
                 container.classList.add('done')
             }
         })
+        return obj;
     })
 }
 
+
 function update() {
+    appelement.scrollTop += max_pixel_per_second
+
     if (pixel_per_second < max_pixel_per_second) pixel_per_second++
+    else pixel_per_second = max_pixel_per_second
+
     requestAnimationFrame(update)
+    console.log('update')
+
+    //
+    // 顯示延遲顯示的物件
+    //
+    delayshows = delayshows.filter(x => !x.classList.contains('show'))
+
+    delayshows.map(x => {
+        rect = x.getBoundingClientRect()
+        if (rect.y < innerHeight - rect.height / 2) {
+            if (x.classList.contains('delay-sequence'))
+                x.style.transitionDelay = rect.x / innerWidth * 0.5 + 0.1 + 's'
+            x.classList.add('show')
+        }
+    })
 
 
     //
     // 讓標籤會卡在最上面
     //
 
-    tags.map(tag => {
-        tag.classList.remove('fixed')
-        tag.rect = tag.anchor.getBoundingClientRect()
-        tag.text.style.top = tag.rect.y + 'px'
-        return tag
-    })
+    // tags.map(tag => {
+    //     tag.classList.remove('fixed')
+    //     tag.rect = tag.anchor.getBoundingClientRect()
+    //     tag.text.style.top = tag.rect.y + 'px'
+    //     return tag
+    // })
 
-    // tags.sort((a, b) => a.rect.y - b.rect.y)
+    // // tags.sort((a, b) => a.rect.y - b.rect.y)
 
-    lastOutTag = null
-    nextOutTag = null
-
-
-    tags.map(tag => {
-        if (tag.rect.y < 0) lastOutTag = tag
-        else if (nextOutTag == null) nextOutTag = tag
-    }).pop()
-
-    if (lastOutTag) {
-        lastOutTag.classList.add('fixed')
-        lastOutTag.text.style.top = '0px'
-    }
+    // lastOutTag = null
+    // nextOutTag = null
 
 
-    if (nextOutTag && lastOutTag) {
-        lastOutTag.text.style.top = Math.min(
-            0, nextOutTag.rect.y - nextOutTag.rect.height
-        ) + 'px'
-    }
+    // tags.map(tag => {
+    //     if (tag.rect.y < 0) lastOutTag = tag
+    //     else if (nextOutTag == null) nextOutTag = tag
+    // }).pop()
+
+    // if (lastOutTag) {
+    //     lastOutTag.classList.add('fixed')
+    //     lastOutTag.text.style.top = '0px'
+    // }
+
+
+    // if (nextOutTag && lastOutTag) {
+    //     lastOutTag.text.style.top = Math.min(
+    //         0, nextOutTag.rect.y - nextOutTag.rect.height
+    //     ) + 'px'
+    // }
 
 }
